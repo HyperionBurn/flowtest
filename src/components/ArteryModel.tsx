a13afcb5c5956b2ad7d0abd35bc90a6892ad955d
Rayan <rayan@aera.in>
2026-05-21 21:57:27 +0400

    Fix missing useMemo import causing blank screen

--- src/components/ArteryModel.tsx
+++ src/components/ArteryModel.tsx
@@ -1,4 +1,4 @@
-import { useRef } from 'react';
+import { useRef, useMemo } from 'react';
 import * as THREE from 'three';
 import { useFrame } from '@react-three/fiber';
 import { useAppStore } from '../store/useAppStore';
